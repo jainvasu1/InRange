2234a4b069d953a7d62aa3f8e30391cd4b22a1c3
jainvasu1 <jainvasu1@users.noreply.github.com>
2026-04-28 15:30:49 +0530

    Trust reverse proxy so secure cookies work behind Render HTTPS

--- server.js
+++ server.js
@@ -23,6 +23,9 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
+// Trust Render/Heroku-style reverse proxy so secure cookies work over HTTPS
+app.set("trust proxy", 1);
+
 // -------- Middleware --------
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
